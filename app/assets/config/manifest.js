2f97da10e91d4fecd7c50c7a40dddbbc37f7f7cf
Victor Costa <70709626+victor9743@users.noreply.github.com>
2025-02-12 16:58:39 -0300

    feature

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,9 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link jquery.min.js
+//= link jquery.easing.min.js
+//= link sb-admin-2.min.js
+//= link Chart.min.js
+//= link chart-area-demo.js
+//= link chart-pie-demo.js
